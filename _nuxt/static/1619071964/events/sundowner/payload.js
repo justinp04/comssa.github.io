af960bd677f83f6088026cf03b1d43e996dff339
Accurate0 <Accurate0@users.noreply.github.com>
2021-04-23 13:54:56 +0000

    deploy: d59a5612304d4e9c9314c290ca1c1db9db4e9d62

--- _nuxt/static/1619071964/events/sundowner/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/events/sundowner", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1619186091/events/feedback/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619186091",layout:"default",error:a,serverRendered:false,routePath:"\u002Fevents\u002Ffeedback",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}}}}(null));
