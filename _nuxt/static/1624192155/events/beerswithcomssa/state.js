3786be5711174de6f2f372ed7abf38580497077c
Accurate0 <Accurate0@users.noreply.github.com>
2021-07-11 08:30:26 +0000

    deploy: 397e05e90217a250c48513e62fd916e30ecec87e

--- _nuxt/static/1624192155/events/beerswithcomssa/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624192155",layout:"default",error:a,serverRendered:false,routePath:"\u002Fevents\u002Fbeerswithcomssa",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}}}}(null));
